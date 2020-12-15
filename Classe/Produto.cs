3e734feb0bb6ce265f663a81cd1b6567867e35f3
DEV <dev@info.local>
2020-12-15 16:58:03 -0300

    Exercício feito

---
+++ Classe/Produto.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace MetodosConstrutores.Classe
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Estoque { get; set; }
+
+        public Produto(int codigo, string nome, string descricao, int estoque)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Descricao = descricao;
+            Estoque = estoque;
+
+            Console.WriteLine($"Código do produto: -> {Codigo} Nome do produto -> {Nome} Descrição do produto -> {Descricao} Estoque do produto -> {Estoque}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MetodosConstrutores.Classe;
+
+namespace MetodosConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(0000000000, "Hot-Dog", "Hot-Dog feito com muito capricho.\r\nNele vai ourê, batata, maionese e o que mais você desejar.", 15);
+        }
+    }
+}
